f3ee35e642f4477be2b8e075ec52bbcef9b4bd53
Debbiejerry <hisdelightdeborah@gmail.com>
2023-08-03 03:15:05 +0100

    first commit

---
+++ UsingVariables.java
@@ -0,0 +1,10 @@
+public class UsingVariables {
+    public static void main(String[] args) {
+		String myName = "Ikongshul Deborah Agianiye";
+		String myGender = "I am a female";
+		int myNumber = 21;
+		String myBio = "I am a Nigerian";
+		System.out.print("Hi, my name is " + myName + " and " + myGender + ". " + myBio + " and i am " + myNumber + " years old.");
+	}
+
+}
