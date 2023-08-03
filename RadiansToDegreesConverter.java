6f98c9853249f3fcf6462517eb5e73321ba8a08b
Debbiejerry <hisdelightdeborah@gmail.com>
2023-08-03 03:23:08 +0100

    first commit

---
+++ RadiansToDegreesConverter.java
@@ -0,0 +1,8 @@
+public class RadiansToDegreesConverter {
+	public static void main(String[] args) {
+		double radians = Math.PI / 8.0;
+		double degrees = Math.toDegrees(radians);
+		
+		System.out.println("PI/8 radians in degrees is: " + radians);
+	}
+}
