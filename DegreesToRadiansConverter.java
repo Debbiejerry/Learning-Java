4d995fb3777f8b44bfb26b319b6d3e12cf7858a4
Debbiejerry <hisdelightdeborah@gmail.com>
2023-08-03 03:24:51 +0100

    first commit

---
+++ DegreesToRadiansConverter.java
@@ -0,0 +1,7 @@
+public class DegreesToRadiansConverter {
+	public static void main(String[] args) {
+		double degrees = 54.0;
+		double radians = Math.toRadians(degrees);
+		System.out.println("54 degrees in radians is: " + radians);
+	}
+}
