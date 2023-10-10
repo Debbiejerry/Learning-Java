710dd5a6f2073307ea5dd5aa4ecdb6c6759d8372
Debbiejerry <hisdelightdeborah@gmail.com>
2023-10-11 00:52:35 +0100

    first commit

---
+++ Exam.java
@@ -0,0 +1,10 @@
+import java.util.Scanner; // import Scanner class
+class mySecondProgram { 
+public static void main(String[] args) { 
+Scanner input = new Scanner(System.in); //create a Scanner object called input
+System.out.println("Enter the first and second numbers separated by enter: \n"); 
+int number1 =input.nextInt(); //invoke the Integer method of the scanner class and accept input from user
+int number2 =input.nextInt(); //invoke the Integer method of the scanner class and accept input from user
+int number3 =input.nextInt();
+System.out.println("Your result is:"); 
+System.out.println(number1 + number2 + number3); }} //display the result
