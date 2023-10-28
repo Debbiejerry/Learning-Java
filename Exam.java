df4d38fb25cc964247bb79b99b7cf1931197d475
Debbiejerry <hisdelightdeborah@gmail.com>
2023-10-28 14:52:49 +0100

    second commit

--- Exam.java
+++ Exam.java
@@ -1,4 +1,5 @@
 import java.util.Scanner; // import Scanner class
+// class declaration
 class mySecondProgram { 
 public static void main(String[] args) { 
 Scanner input = new Scanner(System.in); //create a Scanner object called input
@@ -7,4 +8,4 @@ int number1 =input.nextInt(); //invoke the Integer method of the scanner class a
 int number2 =input.nextInt(); //invoke the Integer method of the scanner class and accept input from user
 int number3 =input.nextInt();
 System.out.println("Your result is:"); 
-System.out.println(number1 + number2 + number3); }} //display the result
+System.out.println(number1 + number2 + number3); }} //displaying the result
